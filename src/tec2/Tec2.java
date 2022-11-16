46a19a1eeeed74c096e655b3f164fe84a086d79e
Marisol G <Marisol G@Marisol_G>
2022-11-16 15:39:10 -0700

    EJERCICIO 1.2

--- src/tec2/Tec2.java
+++ src/tec2/Tec2.java
@@ -15,7 +15,7 @@ public class Tec2 {
      */
     public static void main(String[] args) {
         System.out.println("Hola");
-        
+        System.out.println("Soy marisol");
     }
     
 }
